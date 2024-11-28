39ffce587916a1b05d71d0f8a9e5d08d532c37bf
rudymartinb <rudy.martin.b@gmail.com>
2024-11-28 14:54:25 -0300

    moved coverage dir to var/coverage -- and added to .gitignore

--- tests/autoruntests.sh
+++ tests/autoruntests.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 clear
 
@@ -14,7 +15,7 @@ fi
 inotifywait -m --format %w%f  --exclude "(\.\*|_docs/|.git|nbproject|coverage)" -q -r -e close_write ./  | \
 while read CUAL ; do
     clear
-    time NODE_V8_COVERAGE=/tmp/cca c8 -x tests/ -r html node ./myrunner.js  ./tests/myTestSuite.js
+    time NODE_V8_COVERAGE=/tmp/cca c8  -o var/coverage  -x tests/ -r html node ./myrunner.js  ./tests/myTestSuite.js
         
 done 
 
